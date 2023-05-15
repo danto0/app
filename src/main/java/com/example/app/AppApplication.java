01a1eb715226a8990cda9b7befa9abc4bd9fb6bb
dantolino <david.antolinorivas@igt.com>
2023-05-15 16:03:45 +0200

    vehicle rename

--- src/main/java/com/example/app/AppApplication.java
+++ src/main/java/com/example/app/AppApplication.java
@@ -1,13 +1,18 @@
 package com.example.app;
 
-import org.springframework.boot.SpringApplication;
+import com.example.lib.WRCar;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+//		SpringApplication.run(AppApplication.class, args);
+		setCar();
+	}
+
+	public static void setCar() {
+		WRCar car = new WRCar(4);
 	}
 
 }

89f24bee146c18a4f81877d8828d468f4d0a1343
dantolino <david.antolinorivas@igt.com>
2023-05-16 16:29:45 +0200

    release

--- src/main/java/com/example/app/AppApplication.java
+++ src/main/java/com/example/app/AppApplication.java
@@ -1,7 +1,8 @@
 package com.example.app;
 
-import com.example.lib.WRCar;
+import com.example.lib.WRCarrr;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.google.common.base.Strings;
 
 @SpringBootApplication
 public class AppApplication {
@@ -9,10 +10,11 @@ public class AppApplication {
 	public static void main(String[] args) {
 //		SpringApplication.run(AppApplication.class, args);
 		setCar();
+		Strings.repeat("hi", 4);
 	}
 
 	public static void setCar() {
-		WRCar car = new WRCar(4);
+		WRCar car = new WRCarrr(4);
 	}
 
 }
